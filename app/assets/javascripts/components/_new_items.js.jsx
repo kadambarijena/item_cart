0811fd427af18374ee3132a8ca4c0e298b404374
Kadambari Jena <pinkyjena1993@gmail.com>
2020-02-18 16:32:33 +0530

    Adding a new item
    -Created in two input fields and send them to the server via post
    request
    -Add the form field and the button to handle the submission.

--- app/assets/javascripts/components/_new_items.js.jsx
+++ app/assets/javascripts/components/_new_items.js.jsx
@@ -2,8 +2,10 @@ window.NewItem = createReactClass({
     render() {
       return (
         <div>
-          hello
+          <input ref={this.name} placeholder='Enter the name of the item' />
+          <input ref={this.description} placeholder='Enter a description' />
+          <button>Submit</button>
         </div>
       );
     }
-  });
+});
